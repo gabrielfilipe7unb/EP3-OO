03180c21d9c7dcf67a1fb645d73aff7814aaec5a
Zarbielli <jlfz06@gmail.com>
2018-07-01 06:16:20 -0300

    Adicionando css a pagina principal

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,5 @@ Rails.application.routes.draw do
     resources :departamentos
     resources :produtos, only: [:new, :create, :destroy, :edit, :update]
     get "/produtos/busca" => "produtos#busca", as: :busca_produto
-    root "produtos#index"
+    root "site/home#index"
 end
